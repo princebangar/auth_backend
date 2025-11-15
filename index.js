f8196dd16e8f2177a5dd20a2b391d7775aeb9bda
Handsome <pratikbanger.smtlabs@gmail.com>
2025-11-15 17:59:36 +0530

    added return before response sending

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ require('./Models/db')
 const PORT = process.env.PORT || 8080;
 
 app.get('/ping', (req, res) => {
-    res.send('PONG')
+    return res.send('PONG')
 })
 
 app.use(bodyParser.json());
